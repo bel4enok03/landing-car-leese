24376e339e377a9cefafd6bddd702f9255036aca
bel4enok03 <belskaya-tanyush@mail.ru>
2023-02-14 23:12:35 +0400

    *

--- script.js
+++ script.js
@@ -1,60 +1,140 @@
-$(function () {
-	$('.form-range').on('input', function () {
-		let maxRange = parseInt($(this).attr('max'));
-		let minRange = parseInt($(this).attr('min'));
-		let value = parseInt($(this).val());
-		let percent = ((value - minRange) / (maxRange - minRange)) * 100;
+// Получаем элементы из DOM
+const costInput = document.getElementById('total-cost');
+const costRange = document.getElementById('total-cost-range');
+const percentInput = document.getElementById('total-percent');
+const percentRange = document.getElementById('total-percent-range');
+const periodInput = document.getElementById('total-period');
+const periodRange = document.getElementById('total-period-range');
+const amountEl = document.getElementById('amount-of-lease');
+const paymentEl = document.getElementById('monthly-payment');
 
-		$(this)
-			.siblings('.track')
-			.css('width', percent + '%');
+//стили для ползунка
+const inputRanges = document.querySelectorAll('.input-range');
+const tracks = document.querySelectorAll('.track');
+
+inputRanges.forEach((inputRange, index) => {
+	inputRange.addEventListener('input', function () {
+		const percent = (inputRange.value - inputRange.min) / (inputRange.max - inputRange.min);
+		tracks[index].style.width = percent * 100 + '%';
 	});
 });
 
-const totalCost = document.getElementById('total-cost');
-const totalPercent = document.getElementById('total-percent');
-const totalPeriod = document.getElementById('total-period');
+// Инициализируем значения по умолчанию
+costInput.value = 1000000;
+costRange.value = 1000000;
+percentInput.value = 10;
+percentRange.value = 10;
+periodInput.value = 1;
+periodRange.value = 1;
 
-const totalCostRange = document.getElementById('total-cost-range');
-const totalPercentRange = document.getElementById('total-percent-range');
-const totalPeriodRange = document.getElementById('total-period-range');
-
-const inputsRange = document.querySelectorAll('.input-range');
-
-const amountOfLease = document.querySelector('#amount-of-lease');
-const monthlyPaymant = document.querySelector('#monthly-paymant');
-
-const assignValue = () => {
-	totalCost.value = totalCostRange.value;
-	totalPercent.value = Math.floor((totalPercentRange.value / 100) * totalCost.value);
-	totalPeriod.value = totalPeriodRange.value;
-
-	let totalCostOfNumber = parseFloat(totalCost.value);
-	let totalPercentOfNumber = parseFloat(totalPercent.value);
-	let totalPeriodOfNumber = parseFloat(totalPeriod.value);
-
-	//(Стоимость автомобиля - Первоначальный взнос) * ((Процентная ставка * (1 + Процентная ставка)^Срок кредита в месяцах) / ((1 + Процентная ставка)^Срок кредита в месяцах - 1))
-	//const monthPay = (price - initial) * ((0.035 * Math.pow((1 + 0.035), months)) / (Math.pow((1 + 0.035), months) - 1));
-
-	let initialPayment = Math.floor(
-		(totalCostOfNumber - totalPercentOfNumber) *
-			((0.035 * Math.pow(1 + 0.04, totalPeriodOfNumber)) / (Math.pow(1 + 0.04, totalPeriodOfNumber) - 1))
-	);
-	// Для поля “Сумма договора лизинга”:
-	// Первоначальный взнос + Срок кредита в месяцах * Ежемесячный платеж
-
-	let contractAmount = Math.floor(totalPercentOfNumber + totalPeriodOfNumber * initialPayment);
-	console.log(typeof contractAmount);
-
-	monthlyPaymant.innerHTML = `${initialPayment} Р`;
-	amountOfLease.innerHTML = `${contractAmount} Р`;
-
-};
-
-
-
-for (let input of inputsRange) {
-	input.addEventListener('input', () => {
-		assignValue();
+// Обработчик событий для изменения значения ползунка
+function handleRangeChange(inputEl, rangeEl) {
+	rangeEl.addEventListener('input', () => {
+		inputEl.value = rangeEl.value;
+		updateCalculations();
 	});
 }
+
+// Обработчик событий для изменения значения инпута
+function handleInputChange(inputEl, rangeEl) {
+	inputEl.addEventListener('input', () => {
+		// Если введенное значение находится в диапазоне, то обновляем значение ползунка
+		if (inputEl.value >= rangeEl.min && inputEl.value <= rangeEl.max) {
+			rangeEl.value = inputEl.value;
+			updateCalculations();
+		}
+	});
+}
+
+// Функция для обновления расчетов
+function updateCalculations() {
+	const cost = Number(costInput.value);
+	const percent = Number(percentInput.value) / 100;
+	const period = Number(periodInput.value);
+	const initial = cost * percent;
+	const monthPay = (cost - initial) * ((0.035 * Math.pow(1 + 0.035, period)) / (Math.pow(1 + 0.035, period) - 1));
+	const amount = initial + period * monthPay;
+	const formatter = new Intl.NumberFormat('ru-RU', {
+		style: 'currency',
+		currency: 'RUB',
+	});
+	amountEl.innerText = formatter.format(amount);
+	paymentEl.innerText = formatter.format(monthPay);
+}
+
+// Добавляем обработчики событий на элементы
+handleRangeChange(costInput, costRange);
+handleRangeChange(percentInput, percentRange);
+handleRangeChange(periodInput, periodRange);
+handleInputChange(costInput, costRange);
+handleInputChange(percentInput, percentRange);
+handleInputChange(periodInput, periodRange);
+
+// Обновляем расчеты при инициализации
+updateCalculations();
+
+// отправка данных на сервер
+const buttonEl = document.querySelector('.btn');
+
+function sendFormData() {
+	// Блокируем все инпуты и слайдеры
+	costInput.disabled = true;
+	costRange.disabled = true;
+	percentInput.disabled = true;
+	percentRange.disabled = true;
+	periodInput.disabled = true;
+	periodRange.disabled = true;
+	buttonEl.innerHTML = '<i class="fas fa-spinner fa-spin"></i> Отправка...';
+	buttonEl.disabled = true;
+
+	const cost = Number(costInput.value);
+	const percent = Number(percentInput.value) / 100;
+	const period = Number(periodInput.value);
+	const initial = cost * percent;
+	const monthPay = (cost - initial) * ((0.035 * Math.pow(1 + 0.035, period)) / (Math.pow(1 + 0.035, period) - 1));
+	const amount = initial + period * monthPay;
+
+	const data = {
+		car_coast: cost,
+		initail_payment: initial,
+		initail_payment_percent: percent * 100,
+		lease_term: period,
+		total_sum: amount,
+		monthly_payment_from: monthPay,
+	};
+
+	fetch('https://hookb.in/eK160jgYJ6UlaRPldJ1P', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(data),
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			// Разблокируем все инпуты и слайдеры
+			costInput.disabled = false;
+			costRange.disabled = false;
+			percentInput.disabled = false;
+			percentRange.disabled = false;
+			periodInput.disabled = false;
+			periodRange.disabled = false;
+			buttonEl.innerHTML = 'Оформить заявку';
+			buttonEl.disabled = false;
+			console.log(data);
+		})
+		.catch((error) => {
+			// Разблокируем все инпуты и слайдеры
+			costInput.disabled = false;
+			costRange.disabled = false;
+			percentInput.disabled = false;
+			percentRange.disabled = false;
+			periodInput.disabled = false;
+			periodRange.disabled = false;
+			buttonEl.innerHTML = 'Оформить заявку';
+			buttonEl.disabled = false;
+			console.error('Error:', error);
+		});
+}
+
+buttonEl.addEventListener('click', sendFormData);
